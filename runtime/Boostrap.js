9171e0439e5966eec65c73cf5163d7cee8470ee4
auraecosystem <sandraisom@icloud.com>
2026-05-28 00:13:10 +0100

    Update Boostrap.js

--- runtime/Boostrap.js
+++ runtime/Boostrap.js
@@ -1,17 +1,17 @@
-import { Web4OS } from "./os";
-import { Web4AI } from "./ai";
-import { XLSL } from "./xlsl";
-import { XSIM } from "./xsim";
-import { XQSL } from "./xqsl";
+import { Web4Runtime } from "./web4-runtime.js";
+import { XAI } from "./xai.js";
+import { XLSL } from "./xlsl.js";
+import { XSIM } from "./xsim.js";
+import { XQSL } from "./xqsl.js";
 
-export const runtime = new Web4OS();
+export const runtime = new Web4Runtime();
 
 // register everything
-runtime.register("ai", Web4AI);
+runtime.register("ai", XAI);
 runtime.register("xlsl", XLSL);
 runtime.register("xsim", XSIM);
 runtime.register("xqsl", XQSL);
 
-// system init state
-runtime.set("app.name", "Web4 OS v1");
+// initial system state
+runtime.set("app.name", "Web4 Unified Runtime");
 runtime.set("system.status", "online");
