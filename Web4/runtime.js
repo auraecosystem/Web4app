bc9968211f2fd09151d541115e68d9c833d445ff
auraecosystem <sandraisom@icloud.com>
2026-05-27 23:45:34 +0100

    Create runtime.js

---
+++ Web4/runtime.js
@@ -0,0 +1,9 @@
+import { Web4 } from "./core";
+
+export const web4 = new Web4();
+
+// initial state
+web4.set("app.name", "Web4 Framework");
+web4.set("user.name", "Guest");
+web4.set("data.metricA", 42);
+web4.set("data.metricB", 99);
